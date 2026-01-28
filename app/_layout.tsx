5cbf6e38bb962eabcd2518e804d400b37a40a7cd
Darshan <darshanchoudhary2007@gmail.com>
2026-01-29 00:50:45 +0530

    added logic of requesting bg location permission and sending it to server to store it in db

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,66 @@
+import * as Location from "expo-location";
 import { Stack } from "expo-router";
+import { Platform } from "react-native";
+import "../tasks/LocationTask";
+const LOCATION_TASK = "BACKGROUND_LOCATION_TASK";
+
+
+// this is for background location update in db
+export const startBackgroundLocation = async () => {
+  console.log("Requesting location permissions...");
+  const fg = await Location.requestForegroundPermissionsAsync();
+  if (!fg.granted) {
+    console.log("Foreground location permission denied");
+    return;
+  }
+
+  const bg = await Location.requestBackgroundPermissionsAsync();
+  if (!bg.granted) {
+    console.log(
+      "Please allow background location from Settings for live tracking."
+    );
+    return;
+  }
+
+
+  const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+    LOCATION_TASK
+  );
+  console.log("Has location tracking started:", hasStarted);
+  if (hasStarted) {
+    stopBackgroundLocation();
+  };
+
+  console.log("Starting background location tracking...");
+
+  await Location.startLocationUpdatesAsync(LOCATION_TASK, {
+    accuracy: Location.Accuracy.High,
+    timeInterval: 15000,
+    distanceInterval: 10,
+    pausesUpdatesAutomatically: false,
+    activityType:
+      Platform.OS === "android"
+        ? Location.LocationActivityType.OtherNavigation
+        : undefined,
+    foregroundService: {
+      notificationTitle: "Location Tracking",
+      notificationBody: "Tracking your location in background",
+    },
+  });
+
+  console.log("Background location tracking started");
+};
+
+const stopBackgroundLocation = async () => {
+  const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+    LOCATION_TASK
+  );
+  if (hasStarted) {
+    await Location.stopLocationUpdatesAsync(LOCATION_TASK);
+    console.log("Background location tracking stopped");
+  }
+};
+
 
 export default function Layout() {
   return (
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,11 @@
 import { Redirect } from "expo-router";
-
+import { useEffect } from "react";
+import { startBackgroundLocation } from "./_layout";
 export default function Index() {
+  useEffect(() => {
+    startBackgroundLocation();
+  }, []);
  // return <Redirect href="/registration/EmailScreen" />;
-  return <Redirect href="/client" />;
+  return <Redirect href="/client/WorkerRankingScreen" />;
   // return <Redirect href="/AppWriteOTP" />;
 }

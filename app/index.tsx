e16ec733ea5df22e9f86c97d3a9455fc34ef20d0
Darshan <darshanchoudhary2007@gmail.com>
2026-02-07 22:47:42 +0530

    merged updates from shreyash's code

--- app/index.tsx
+++ app/index.tsx
@@ -98,8 +98,8 @@ export default function Index() {
   }, []);
 
   // return <Redirect href="/registration/EmailScreen" />;
-  return <Redirect href="/client/WorkerRankingScreen" />;
-  // return <Redirect href="/client/" />;
+  // return <Redirect href="/client/WorkerRankingScreen" />;
+  return <Redirect href="/client/" />;
   // return <Redirect href="/client/JobRequest" />;
   // return <Redirect href="/client/CommunicationRoom" />;
   // return <Redirect href="/call/CallingScreen" />;

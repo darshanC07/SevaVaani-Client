968ca58f7954a71dd3fd4f6b09325ea4c389ebd3
Darshan <darshanchoudhary2007@gmail.com>
2026-03-28 17:28:32 +0530

    added message when intent is OTHER

--- components/AIChatOverlay.tsx
+++ components/AIChatOverlay.tsx
@@ -315,7 +315,18 @@ const AIChatOverlay = ({ onClose }: { onClose: () => void }) => {
 
           router.push("/client/WorkerRankingScreen");
         }
-      } else if (intent === "other") { }
+      } else if (intent === "other") {
+        addMessage("I'm sorry, I can only help with posting jobs or listing nearby workers for now.", "ai");
+        await TTS_module.getMsg(
+          "I'm sorry, I can only help with posting jobs or listing nearby workers for now."
+        );
+       }
+        else {
+        addMessage("I'm sorry, I didn't understand that. Could you please rephrase?", "ai");
+        await TTS_module.getMsg(
+          "I'm sorry, I didn't understand that. Could you please rephrase?"
+        );
+      }
 
     } catch (error: any) {
       Alert.alert("Error", error?.message ?? String(error));
